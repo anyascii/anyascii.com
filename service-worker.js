fce33ce8a1910a577fcd6dc8c9367b31b3aa9c62
hunterwb <hunter@hunterwb.com>
2024-10-02 15:37:45 -0700

    cache ignoreSearch

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch', event => {
 	if (event.request.url.startsWith(self.location.origin)) {
 		event.respondWith(
 			caches.open(CACHE_NAME).then(cache => {
-				return cache.match(event.request).then(response => {
+				return cache.match(event.request, {ignoreSearch: true}).then(response => {
 					if (response) return response;
 					return fetch(event.request).then(response => {
 						cache.put(event.request, response.clone());
