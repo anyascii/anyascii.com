1f81ef641b65cef92f5e984821cbffc628a39934
hunterwb <hunter@hunterwb.com>
2024-10-02 14:21:27 -0700

    Start service worker

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'AnyAscii-v1';
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.addAll([
+				'/',
+				'/index.js',
+			]);
+		}),
+	);
+});
+
+self.addEventListener('fetch', event => {
+	if (event.request.url.startsWith(self.location.origin)) {
+		event.respondWith(
+			caches.open(CACHE_NAME).then(cache => {
+				return cache.match(event.request).then(response => {
+					if (response) return response;
+					return fetch(event.request).then(response => {
+						cache.put(event.request, response.clone());
+						return response;
+					});
+				});
+			}),
+		);
+	}
+});
